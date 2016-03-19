7dfee8eb9b4f27f33c1df2b1ab8295922e2dc954
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-19 11:57:52 +0100

    Extract Urho3D in same folder

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 cd ..
 git clone https://github.com/richelbilderbeek/RibiClasses
 git clone https://github.com/richelbilderbeek/RibiLibraries
-ls
-cd RibiLibraries
-./Urho3d.sh
-cd ..
 cd RosindellEtAl2008
+./Urho3d.sh
 qmake RosindellEtAl2008Console.pro
 make
 qmake RosindellEtAl2008Desktop.pro
